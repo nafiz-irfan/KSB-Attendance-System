ae1518a2f008e86870ee27e17ab92d65e2f09563
mYs26 <amirfarhan.97@gmail.com>
2024-02-24 18:25:12 +0800

    remove setting, adjust active navbar

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -15,7 +15,7 @@
       </footer>
     </div>
   </main>
-  <div class="fixed-plugin">
+  {{-- <div class="fixed-plugin">
     <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
       <i class="material-icons py-2">settings</i>
     </a>
@@ -88,7 +88,7 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> --}}
   <!--   Core JS Files   -->
   <script src="../assets/js/core/popper.min.js"></script>
   <script src="../assets/js/core/bootstrap.min.js"></script>
